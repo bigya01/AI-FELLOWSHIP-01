26becfe630568196cdd91e6113ce333d1482e076
bigya01 <078bct032.bindu@pcampus.edu.np>
2025-05-15 21:17:02 +0545

    use environment-based-configuration

---
+++ app/core/config.py
@@ -0,0 +1,10 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    app_name: str = "FastAPI App"
+    env: str = "development"
+    port: int = 8000
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from app.api import health
+from app.core.config import settings
 
-app = FastAPI()
+app = FastAPI(title=settings.app_name)
+
 
 app.include_router(health.router)
 
 @app.get("/")
 def root():
-    return {"message": "Welcome to the URL Shortener"}
+       return { "message": f"Welcome to {settings.app_name} running in {settings.env} mode."}
